cb13de5dc90f8bbc375c46f5fb7f9efcd0fd0709
michock.mit <michock.mit@gmail.com>
2021-06-26 18:23:20 -0500

    from weldmicheal

--- src/main/java/Domain/Entity/Visit.java
+++ src/main/java/Domain/Entity/Visit.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 import java.util.Date;
-
+//
 @Component
 @Entity(name = "visits")
 public class Visit {
@@ -12,7 +12,7 @@ public class Visit {
     @Column(name = "Id")
     private int visitId;
     @Column(name = "visitDate")
-    private Date visistDate;
+    private Date visitDate;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="patientId",referencedColumnName = "Id")
