977343b703137697b82a992c5d8ee4c548d52e42
kidst <mezgebekidst@gmail.com>
2021-06-27 14:59:25 -0500

    added class labtesttype and systemadmin

--- src/main/java/Domain/Entity/SystemAdmin.java
+++ src/main/java/Domain/Entity/SystemAdmin.java
@@ -3,6 +3,7 @@ package Domain.Entity;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
+import java.util.*;
 
 @Component
 @Entity(name="SystemAdmins")
@@ -11,7 +12,62 @@ public class SystemAdmin {
     @Id
     @Column(name = "Id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long systemAdminId;
+    private int systemAdminId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="userId",referencedColumnName = "Id")
+    private User user;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="labTestTypeId", referencedColumnName = "Id")
+    private List <LabTestType> labTestTypeList;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="drugId", referencedColumnName = "Id")
+    private List<Drug> drugList;
+
+    public SystemAdmin(int systemAdminId, User user, List<LabTestType> labTestType, List<Drug> drug) {
+        this.systemAdminId = systemAdminId;
+        this.user = user;
+        this.labTestTypeList = labTestType;
+        this.drugList = drug;
+    }
+
+    public SystemAdmin(User user, List<LabTestType> labTestTypeList, List<Drug> drugList) {
+        this.user = user;
+        this.labTestTypeList = labTestTypeList;
+        this.drugList = drugList;
+    }
+
+    public SystemAdmin() {
+    }
+
+    public int getSystemAdminId() {
+        return systemAdminId;
+    }
 
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<LabTestType> getLabTestType() {
+        return labTestTypeList;
+    }
+
+    public void setLabTestType(List<LabTestType> labTestType) {
+        this.labTestTypeList = labTestType;
+    }
+
+    public List<Drug> getDrug() {
+        return drugList;
+    }
+
+    public void setDrug(List<Drug> drug) {
+        this.drugList = drug;
+    }
 }
