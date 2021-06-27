40a5b3d8a6b373efb6af2f2669abd515c8c5cc06
abebe <abebetekle@gmail.com>
2021-06-27 10:31:39 -0500

    Drug and Laboratorist added

--- src/main/java/Domain/Entity/Drug.java
+++ src/main/java/Domain/Entity/Drug.java
@@ -1,7 +1,35 @@
 package Domain.Entity;
 
+import net.minidev.json.annotate.JsonIgnore;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.*;
+
 @Component
+@Entity(name="Drugs")
 public class Drug {
+    @Id
+    @Column(name="Id")
+    private Long drugId;
+
+    private String drugName;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="systemAdmin_id")
+    private SystemAdmin systemAdmin;
+
+    public Drug(Long drugId, String drugName, SystemAdmin systemAdmin) {
+        this.drugId = drugId;
+        this.drugName = drugName;
+        this.systemAdmin = systemAdmin;
+    }
+
+    public Drug(String drugName, SystemAdmin systemAdmin) {
+        this.drugName = drugName;
+        this.systemAdmin = systemAdmin;
+    }
+
+    public Drug() {
+    }
+
 }
--- src/main/java/Domain/Entity/Laboratorist.java
+++ src/main/java/Domain/Entity/Laboratorist.java
@@ -1,7 +1,43 @@
 package Domain.Entity;
 
+import net.minidev.json.annotate.JsonIgnore;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 @Component
+@Entity(name = "Laboratorists")
 public class Laboratorist {
+
+    @Id
+    @Column(name = "Id")
+    private int laboratoristId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "UserId", referencedColumnName = "Id")
+    private User user;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "laboratorist") // to be copied from LabResult class
+    private List<LabResult> labResultList = new LinkedList<>();
+
+    public Laboratorist(int laboratoristId, User user, List<LabResult> labResultList) {
+        this.laboratoristId = laboratoristId;
+        this.user = user;
+        this.labResultList = labResultList;
+    }
+
+    public Laboratorist(User user, List<LabResult> labResultList) {
+        this.user = user;
+        this.labResultList = labResultList;
+    }
+
+    public Laboratorist() {
+    }
+
+
 }
