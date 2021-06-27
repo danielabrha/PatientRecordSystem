3c84c94cf0aa37c231b5757298ebf0e8dcdb0b1d
Gizachew <fwefe@gmail.com>
2021-06-27 14:43:57 -0500

    Lab result Entity is under finalizing

--- src/main/java/Domain/Entity/DrugOrder.java
+++ src/main/java/Domain/Entity/DrugOrder.java
@@ -1,9 +1,40 @@
 package Domain.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
 @Component
+@Entity
+@Table(name = "drugOrder")
 public class DrugOrder {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int Id;
+private int amount;
+public DrugOrder() {
+}
+public int getAmount() {
+    return amount;
+}
+public void setAmount(int amount) {
+    this.amount = amount;
+}
+public DrugOrder(int id, int amount) {
+    Id = id;
+    this.amount = amount;
+}
+public int getId() {
+    return Id;
+}
+public void setId(int id) {
+    Id = id;
+}
+
 
     // This is from doctor drug
 }
--- src/main/java/Domain/Entity/LabResult.java
+++ src/main/java/Domain/Entity/LabResult.java
@@ -1,7 +1,56 @@
 package Domain.Entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
 @Component
+@Entity
+@Table(name = "labResult")
 public class LabResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="Id")
+    private int Id;
+
+    @Column(name = "result")
+    private String result;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "laboratoristId",referencedColumnName = "Id")
+    private Laboratorist laboratorist;
+
+
+    public LabResult() {
+    }
+    public LabResult(int id, String result) {
+        Id = id;
+        this.result = result;
+    }
+    public LabResult(String result) {
+        this.result = result;
+ 
+   }
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+    public String getResult() {
+        return result;
+    }
+    public void setResult(String result) {
+        this.result = result;
+    }
+    
+
 }
