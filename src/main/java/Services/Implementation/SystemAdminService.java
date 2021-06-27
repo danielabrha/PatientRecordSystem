7cc32ae13e1a22c3f776d730078c0797459eae56
kidst <mezgebekidst@gmail.com>
2021-06-27 18:20:18 -0500

    changes

--- src/main/java/Services/Implementation/SystemAdminService.java
+++ src/main/java/Services/Implementation/SystemAdminService.java
@@ -1,10 +1,8 @@
 package Services.Implementation;
-
 import Domain.Entity.SystemAdmin;
 import Domain.ViewModel.SystemAdminViewModel;
 import Services.Interface.ISystemAdmin;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 @Service
 public class SystemAdminService implements ISystemAdmin {
@@ -12,47 +10,34 @@ public class SystemAdminService implements ISystemAdmin {
     public List<SystemAdmin> findAll() {
         return null;
     }
-
     @Override
     public List<SystemAdmin> findAll(String status) {
         return null;
     }
-
     @Override
     public SystemAdmin findById(int id) {
         return null;
     }
-
     @Override
-    public SystemAdminViewModel update(SystemAdminViewModel systemAdminViewModel) {
+    public SystemAdmin update(SystemAdminViewModel systemAdminViewModel) {
         return null;
     }
-
     @Override
     public void deleteById(int id) {
-
     }
-
     @Override
     public void delete(SystemAdminViewModel systemAdminViewModel) {
-
     }
-
     @Override
     public void deleteAll(Iterable<SystemAdminViewModel> systemAdminViewModels) {
-
     }
-
     @Override
     public void deleteAll() {
-
     }
-
     @Override
     public SystemAdmin create(SystemAdminViewModel systemAdminViewModel) {
         return null;
     }
-
     @Override
     public List<SystemAdmin> createAll(List<SystemAdminViewModel> listSystemAdminViewModel) {
         return null;
