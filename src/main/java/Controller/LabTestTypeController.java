159e6f1f324519c94839b4644fa3760431dd3d68
kidst <mezgebekidst@gmail.com>
2021-06-29 01:34:51 -0500

    changes

--- src/main/java/Controller/LabTestTypeController.java
+++ src/main/java/Controller/LabTestTypeController.java
@@ -1,7 +1,32 @@
 package Controller;
 
+import Domain.ViewModel.LabTestTypeViewModel;
+import Domain.ViewModel.SystemAdminViewModel;
+import Services.Implementation.LabTestType;
+import Services.Interface.ILabTestType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LabTestTypeController {
+    private ILabTestType labTestTypeService;
+    private LabTestTypeViewModel labTestTypeViewModel;
+    private LabTestType labTestType;
+
+    @RequestMapping("LabTestType/post/data")
+     public LabTestTypeViewModel postSystemAdmin(@RequestBody LabTestTypeViewModel labTestTypeViewModel){
+        return toLabTestTypeViewModel(labTestTypeService.create(labTestTypeViewModel));
+    }
+    @RequestMapping("LabTestType/update/data")
+    public LabTestTypeViewModel update(@RequestBody LabTestTypeViewModel labTestTypeViewModel){
+      return  labTestTypeService.update(labTestTypeViewModel);
+    }
+    public LabTestTypeViewModel toLabTestTypeViewModel(Domain.Entity.LabTestType labTestType){
+        labTestTypeViewModel.setLabTestTypeViewModel_Id(labTestType.getLabTestTypeId());
+        labTestTypeViewModel.setLabTestViewModelName(labTestType.getLabTestName());
+        labTestTypeViewModel.setLabTestViewModelCode(labTestType.getLabTestCode());
+        return labTestTypeViewModel;
+    }
+
 }
